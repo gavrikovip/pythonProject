d218dae705dbcb8aecfd1e1430990933522cb4a4
Gavrikov Ivan <gipard6@gmail.com>
2022-04-17 15:23:33 +0500

    Первые 3 задачи

---
+++ Exersizes/002.py
@@ -0,0 +1,24 @@
+# Найти максимальное из пяти чисел
+print('Введите через enter 5 чисел')
+firstnumber = int(input())
+secondnumber = int(input())
+thirtnumber = int(input())
+fourthnumber = int(input())
+fivethnumber = int(input())
+
+print(max(3, 13, 76, 23, 234,))
+max = 0
+
+if max < firstnumber:
+    max = firstnumber
+if max < secondnumber:
+    max = secondnumber
+if max < thirtnumber:
+    max = thirtnumber
+if max < fourthnumber:
+    max = fourthnumber
+if max < fivethnumber:
+    max = fivethnumber
+print(f'Максимальное число = {max}')
+
+
